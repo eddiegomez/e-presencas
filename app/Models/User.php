791b9b79ef3041b96ffc41cdd677fe2a9daee808
Edson Gomes <Edson Gomes>
2024-10-10 10:46:24 -0100

    Role function para facilitar na verificação de protocolos no acto da parcação de presenças

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
   {
     return $this->belongsTo(Organization::class);
   }
+
+  public function roles()
+  {
+    return $this->belongsToMany(Role::class, 'model_has_roles', 'model_id', 'role_id');
+  }
 }

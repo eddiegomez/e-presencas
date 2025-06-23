5e664ad126174b41ddd5a6a683a87cceb211ef30
Edson Gomes <Edson Gomes>
2025-06-23 15:10:51 -0100

    Correção do feedback do registo de participante em evento

--- resources/views/events/details.blade.php
+++ resources/views/events/details.blade.php
@@ -27,7 +27,6 @@
                 @if(session('success'))
                 <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
                     {{ session('success') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fechar"></button>
                 </div>
                 @endif
 
